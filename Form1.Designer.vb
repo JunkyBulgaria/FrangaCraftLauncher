bd5a8ea83c17a916fa948240548340fa9c53ddf8
franga2000 <miha.frangez@gmail.com>
2013-12-16 20:00:37 +0100

    Beta 1.0
    Full changelog:
    - Now can actually donwload the modpack
    - Cracked Minecraft works
    - Added (useless) options
    - Added a bounch of buttons that don't do anything
    
    Known bugs:
    - Not having internet connection crashes it

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -38,17 +38,36 @@ Partial Class Launcher
         Me.CheckBox1 = New System.Windows.Forms.CheckBox()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.TabPage2 = New System.Windows.Forms.TabPage()
-        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TxtBox_CrackedUser = New System.Windows.Forms.TextBox()
         Me.Label4 = New System.Windows.Forms.Label()
         Me.Button3 = New System.Windows.Forms.Button()
         Me.Label5 = New System.Windows.Forms.Label()
         Me.PremLoginBox = New System.Windows.Forms.GroupBox()
         Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Btn_save_settings = New System.Windows.Forms.Button()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TxtBox_RAM = New System.Windows.Forms.TextBox()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Btn_about = New System.Windows.Forms.Button()
+        Me.Btn_credits = New System.Windows.Forms.Button()
+        Me.Btn_MP_info = New System.Windows.Forms.Button()
+        Me.Btn_GitHub = New System.Windows.Forms.Button()
+        Me.Btn_Website = New System.Windows.Forms.Button()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.TabControl1.SuspendLayout()
         Me.TabPage1.SuspendLayout()
         Me.TabPage2.SuspendLayout()
         Me.PremLoginBox.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
         Me.SuspendLayout()
         '
         'ToolTip1
@@ -145,7 +164,7 @@ Partial Class Launcher
         '
         'TabPage2
         '
-        Me.TabPage2.Controls.Add(Me.TextBox3)
+        Me.TabPage2.Controls.Add(Me.TxtBox_CrackedUser)
         Me.TabPage2.Controls.Add(Me.Label4)
         Me.TabPage2.Controls.Add(Me.Button3)
         Me.TabPage2.Controls.Add(Me.Label5)
@@ -153,10 +172,10 @@ Partial Class Launcher
         resources.ApplyResources(Me.TabPage2, "TabPage2")
         Me.TabPage2.Name = "TabPage2"
         '
-        'TextBox3
+        'TxtBox_CrackedUser
         '
-        resources.ApplyResources(Me.TextBox3, "TextBox3")
-        Me.TextBox3.Name = "TextBox3"
+        resources.ApplyResources(Me.TxtBox_CrackedUser, "TxtBox_CrackedUser")
+        Me.TxtBox_CrackedUser.Name = "TxtBox_CrackedUser"
         '
         'Label4
         '
@@ -190,11 +209,133 @@ Partial Class Launcher
         Me.RichTextBox1.Name = "RichTextBox1"
         Me.RichTextBox1.ReadOnly = True
         '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label9)
+        Me.GroupBox1.Controls.Add(Me.TextBox3)
+        Me.GroupBox1.Controls.Add(Me.Label8)
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.Label7)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.Btn_save_settings)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.TxtBox_RAM)
+        resources.ApplyResources(Me.GroupBox1, "GroupBox1")
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.TabStop = False
+        '
+        'Label9
+        '
+        resources.ApplyResources(Me.Label9, "Label9")
+        Me.Label9.Name = "Label9"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.AutoCompleteCustomSource.AddRange(New String() {resources.GetString("TextBox3.AutoCompleteCustomSource"), resources.GetString("TextBox3.AutoCompleteCustomSource1"), resources.GetString("TextBox3.AutoCompleteCustomSource2"), resources.GetString("TextBox3.AutoCompleteCustomSource3"), resources.GetString("TextBox3.AutoCompleteCustomSource4"), resources.GetString("TextBox3.AutoCompleteCustomSource5"), resources.GetString("TextBox3.AutoCompleteCustomSource6"), resources.GetString("TextBox3.AutoCompleteCustomSource7")})
+        Me.TextBox3.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend
+        Me.TextBox3.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource
+        resources.ApplyResources(Me.TextBox3, "TextBox3")
+        Me.TextBox3.Name = "TextBox3"
+        '
+        'Label8
+        '
+        resources.ApplyResources(Me.Label8, "Label8")
+        Me.Label8.Name = "Label8"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.AutoCompleteCustomSource.AddRange(New String() {resources.GetString("TextBox2.AutoCompleteCustomSource"), resources.GetString("TextBox2.AutoCompleteCustomSource1"), resources.GetString("TextBox2.AutoCompleteCustomSource2"), resources.GetString("TextBox2.AutoCompleteCustomSource3"), resources.GetString("TextBox2.AutoCompleteCustomSource4"), resources.GetString("TextBox2.AutoCompleteCustomSource5"), resources.GetString("TextBox2.AutoCompleteCustomSource6"), resources.GetString("TextBox2.AutoCompleteCustomSource7")})
+        Me.TextBox2.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend
+        Me.TextBox2.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource
+        resources.ApplyResources(Me.TextBox2, "TextBox2")
+        Me.TextBox2.Name = "TextBox2"
+        '
+        'Label7
+        '
+        resources.ApplyResources(Me.Label7, "Label7")
+        Me.Label7.Name = "Label7"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.AutoCompleteCustomSource.AddRange(New String() {resources.GetString("TextBox1.AutoCompleteCustomSource"), resources.GetString("TextBox1.AutoCompleteCustomSource1"), resources.GetString("TextBox1.AutoCompleteCustomSource2"), resources.GetString("TextBox1.AutoCompleteCustomSource3"), resources.GetString("TextBox1.AutoCompleteCustomSource4"), resources.GetString("TextBox1.AutoCompleteCustomSource5"), resources.GetString("TextBox1.AutoCompleteCustomSource6"), resources.GetString("TextBox1.AutoCompleteCustomSource7")})
+        Me.TextBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend
+        Me.TextBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource
+        resources.ApplyResources(Me.TextBox1, "TextBox1")
+        Me.TextBox1.Name = "TextBox1"
+        '
+        'Btn_save_settings
+        '
+        resources.ApplyResources(Me.Btn_save_settings, "Btn_save_settings")
+        Me.Btn_save_settings.Name = "Btn_save_settings"
+        Me.Btn_save_settings.UseVisualStyleBackColor = True
+        '
+        'Label6
+        '
+        resources.ApplyResources(Me.Label6, "Label6")
+        Me.Label6.Name = "Label6"
+        '
+        'TxtBox_RAM
+        '
+        Me.TxtBox_RAM.AutoCompleteCustomSource.AddRange(New String() {resources.GetString("TxtBox_RAM.AutoCompleteCustomSource"), resources.GetString("TxtBox_RAM.AutoCompleteCustomSource1"), resources.GetString("TxtBox_RAM.AutoCompleteCustomSource2"), resources.GetString("TxtBox_RAM.AutoCompleteCustomSource3"), resources.GetString("TxtBox_RAM.AutoCompleteCustomSource4"), resources.GetString("TxtBox_RAM.AutoCompleteCustomSource5"), resources.GetString("TxtBox_RAM.AutoCompleteCustomSource6"), resources.GetString("TxtBox_RAM.AutoCompleteCustomSource7")})
+        Me.TxtBox_RAM.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend
+        Me.TxtBox_RAM.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource
+        resources.ApplyResources(Me.TxtBox_RAM, "TxtBox_RAM")
+        Me.TxtBox_RAM.Name = "TxtBox_RAM"
+        '
+        'ProgressBar1
+        '
+        resources.ApplyResources(Me.ProgressBar1, "ProgressBar1")
+        Me.ProgressBar1.Name = "ProgressBar1"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Btn_about)
+        Me.GroupBox2.Controls.Add(Me.Btn_credits)
+        Me.GroupBox2.Controls.Add(Me.Btn_MP_info)
+        Me.GroupBox2.Controls.Add(Me.Btn_GitHub)
+        Me.GroupBox2.Controls.Add(Me.Btn_Website)
+        resources.ApplyResources(Me.GroupBox2, "GroupBox2")
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.TabStop = False
+        '
+        'Btn_about
+        '
+        resources.ApplyResources(Me.Btn_about, "Btn_about")
+        Me.Btn_about.Name = "Btn_about"
+        Me.Btn_about.UseVisualStyleBackColor = True
+        '
+        'Btn_credits
+        '
+        resources.ApplyResources(Me.Btn_credits, "Btn_credits")
+        Me.Btn_credits.Name = "Btn_credits"
+        Me.Btn_credits.UseVisualStyleBackColor = True
+        '
+        'Btn_MP_info
+        '
+        resources.ApplyResources(Me.Btn_MP_info, "Btn_MP_info")
+        Me.Btn_MP_info.Name = "Btn_MP_info"
+        Me.Btn_MP_info.UseVisualStyleBackColor = True
+        '
+        'Btn_GitHub
+        '
+        resources.ApplyResources(Me.Btn_GitHub, "Btn_GitHub")
+        Me.Btn_GitHub.Name = "Btn_GitHub"
+        Me.Btn_GitHub.UseVisualStyleBackColor = True
+        '
+        'Btn_Website
+        '
+        resources.ApplyResources(Me.Btn_Website, "Btn_Website")
+        Me.Btn_Website.Name = "Btn_Website"
+        Me.Btn_Website.UseVisualStyleBackColor = True
+        '
         'Launcher
         '
         resources.ApplyResources(Me, "$this")
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackgroundImage = Global.FrangaCraftLauncher.My.Resources.Resources.body_bg1
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.GroupBox2)
         Me.Controls.Add(Me.RichTextBox1)
         Me.Controls.Add(Me.PremLoginBox)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
@@ -206,6 +347,9 @@ Partial Class Launcher
         Me.TabPage2.ResumeLayout(False)
         Me.TabPage2.PerformLayout()
         Me.PremLoginBox.ResumeLayout(False)
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
@@ -222,12 +366,29 @@ Partial Class Launcher
     Friend WithEvents Button2 As System.Windows.Forms.Button
     Friend WithEvents Label3 As System.Windows.Forms.Label
     Friend WithEvents TabPage2 As System.Windows.Forms.TabPage
-    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents TxtBox_CrackedUser As System.Windows.Forms.TextBox
     Friend WithEvents Label4 As System.Windows.Forms.Label
     Friend WithEvents Button3 As System.Windows.Forms.Button
     Friend WithEvents Label5 As System.Windows.Forms.Label
     Friend WithEvents Button4 As System.Windows.Forms.Button
     Friend WithEvents PremLoginBox As System.Windows.Forms.GroupBox
     Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents TxtBox_RAM As System.Windows.Forms.TextBox
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Btn_save_settings As System.Windows.Forms.Button
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents Btn_about As System.Windows.Forms.Button
+    Friend WithEvents Btn_credits As System.Windows.Forms.Button
+    Friend WithEvents Btn_MP_info As System.Windows.Forms.Button
+    Friend WithEvents Btn_GitHub As System.Windows.Forms.Button
+    Friend WithEvents Btn_Website As System.Windows.Forms.Button
 
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,57 +1,96 @@
 ﻿Imports System.IO
 Imports System.IO.Compression
+Imports System.Net
 
 Public Class Launcher
+    Public Function GetSettingItem(ByVal file As String, ByVal identifier As String)
+        Dim sr As New IO.StreamReader(file) : Dim result As String = ""
+        Do While (sr.Peek <> -1)
+            Dim line As String = sr.ReadLine
+            If line.ToLower.StartsWith(identifier.ToLower & ":") Then
+                result = line.Substring(identifier.Length + 2)
+            End If
+        Loop
+        Return result
+    End Function
+    Dim WithEvents webClient As New System.Net.WebClient
     Dim InstallPath As String = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) & "\.FrangaCraft"
     Dim closeLauncher As Boolean = False
+    Dim latestMP As String = webClient.DownloadString("https://dl.dropboxusercontent.com/u/23206778/FrangaCraft/FrangaCraft%20Launcher/Data/latest")
 
+    Private Sub Launcher_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'MsgBox(latestMP, , "latestMp")
+    End Sub
+
+    Private Sub Download()
+        ProgressBar1.Show()
+        webClient.DownloadFileAsync(New Uri("https://dl.dropboxusercontent.com/u/23206778/FrangaCraft/FrangaCraft%20Launcher/Data/" & latestMP & ".zip"), InstallPath & " \download.zip")
+    End Sub
+    Private Sub webClient_DownloadProgressChanged(ByVal sender As Object, ByVal e As DownloadProgressChangedEventArgs) Handles webClient.DownloadProgressChanged
+        ProgressBar1.Value = e.ProgressPercentage
+    End Sub
+    Private Sub webClient_DownloadFileCompleted() Handles webClient.DownloadFileCompleted
+        ZipFile.ExtractToDirectory(InstallPath & "\download.zip", InstallPath)
+        ProgressBar1.Value = 0
+        ProgressBar1.Hide()
+        My.Computer.FileSystem.DeleteFile(InstallPath & "\download.zip")
+        MsgBox("The modpack was succesfully downloaded." & vbNewLine & "You can start playing now!", , "Download completed!")
+    End Sub
+    Private Sub Update()
+        Dim currentMP As String = My.Computer.FileSystem.ReadAllText(InstallPath & "\version")
+        ProgressBar1.Show()
+        webClient.DownloadFileAsync(New Uri("https://dl.dropboxusercontent.com/u/23206778/FrangaCraft/FrangaCraft%20Launcher/Data/" & currentMP & ".zip"), InstallPath & " \download.zip")
+        ZipFile.ExtractToDirectory(InstallPath & "\download.zip", InstallPath)
+    End Sub
     Private Sub PictureBox1_Click_1(sender As Object, e As EventArgs) Handles PictureBox1.Click
         System.Diagnostics.Process.Start("http://mc.franga2000.com")
     End Sub
 
     Private Sub PremLogin_Click(sender As Object, e As EventArgs) Handles PremLogin.Click
-        If My.Computer.FileSystem.FileExists(InstallPath & "\version") Then
-            'If the modpack is installed
+StartPrem:
+        If My.Computer.FileSystem.DirectoryExists(InstallPath) Then
+            If File.Exists(InstallPath & "\version") Then
+                Dim currentMP As String = My.Computer.FileSystem.ReadAllText(InstallPath & "\version")
+                If Not (Convert.ToInt32(currentMP) < Convert.ToInt32(latestMP)) Then
+                    Dim WinUser As String = System.Environment.UserName
+                    Dim result As String = webClient.DownloadString("http://login.minecraft.net/?user=" & PremUser.Text() & "&password=" & PremPass.Text & "&version=13")
+                    If Not (result = "Bad login") Then
+                        MsgBox(result, , "result")
+                        Dim result_token As Array = Split(result, ":", 4)
+                        Dim result_mcUser As Array = Split(result, ":", 5)
+                        Dim token As String = result_token(3)
+                        Dim mcUser As String = result_mcUser(2)
+                        MsgBox(token, , "token")
+                        MsgBox(mcUser, , "mcUser")
+                        Dim McWidth As String = "1280"
+                        Dim McHeight As String = "720"
+                        Dim RAM As String = "1G"
+                        Dim run As String = InstallPath & "\1STUFF\direct.bat " & InstallPath & " " & mcUser & " " & token & " " & RAM
+                        'My.Computer.FileSystem.WriteAllText("./run.bat", run, True)
+                        Shell(run, vbNormalFocus)
+                        If closeLauncher Then
 
-            Dim WinUser As String = System.Environment.UserName
-            Dim webClient As New System.Net.WebClient
-            Dim result As String = webClient.DownloadString("http://login.minecraft.net/?user=" & PremUser.Text() & "&password=" & PremPass.Text & "&version=13")
-            If Not (result = "Bad login") Then
-                MsgBox(result, , "result")
-                Dim result_token As Array = Split(result, ":", 4)
-                Dim result_mcUser As Array = Split(result, ":", 5)
-                Dim token As String = result_token(3)
-                Dim mcUser As String = result_mcUser(2)
-                MsgBox(token, , "token")
-                MsgBox(mcUser, , "mcUser")
-                Dim McWidth As String = "1280"
-                Dim McHeight As String = "720"
-                Dim RAM As String = "1G"
-                Dim run As String = InstallPath & "\1STUFF\direct.bat " & InstallPath & " " & mcUser & " " & token & " " & RAM
-                My.Computer.FileSystem.WriteAllText("./run.bat", run, True)
-                Shell(run, vbNormalFocus)
-                If closeLauncher Then
+                            Me.Close()
+                        End If
+                    Else
+                        MsgBox("Bad login!", , "Error")
+                    End If
 
-                    Me.Close()
+                Else
+
+                    'If the modpack isn't installed
+                    If MsgBox("You don't have the latest version of the modpack! Want to update?", MsgBoxStyle.YesNo, "Error") = MsgBoxResult.Yes Then
+                        Update()
+                    End If
                 End If
             Else
-                MsgBox("Bad login!", , "Error")
-            End If
-
-        Else
-
-            'If the modpack isn't installed
-            If MsgBox("The modpack isn't installed. Nothing you can do about it (yet) :P", MsgBoxStyle.YesNo, "Error") = MsgBoxResult.Yes Then
-                'Install the modpack
-InstallPack:
-                If My.Computer.FileSystem.FileExists(InstallPath) Then
-
-                    ZipFile.ExtractToDirectory(InstallPath & "\download.zip", InstallPath)
-                Else
-                    My.Computer.FileSystem.CreateDirectory(InstallPath)
-                    GoTo InstallPack
+                If MsgBox("You don't have the modpack! Want to install it?", MsgBoxStyle.YesNo, "Error") = MsgBoxResult.Yes Then
+                    Download()
                 End If
             End If
+        Else
+            My.Computer.FileSystem.CreateDirectory(InstallPath)
+            GoTo StartPrem
         End If
     End Sub
 
@@ -60,5 +99,71 @@ InstallPack:
             e.SuppressKeyPress = True
             PremLogin_Click(sender, New System.EventArgs())
         End If
+    End Sub
+    Private Sub TxtBox_CrackedUser_KeyDown(sender As Object, e As KeyEventArgs) Handles TxtBox_CrackedUser.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            e.SuppressKeyPress = True
+            Button3_Click(sender, New System.EventArgs())
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If My.Computer.FileSystem.DirectoryExists(InstallPath) Then
+            If File.Exists(InstallPath & "\version") Then
+                Dim currentMP As String = My.Computer.FileSystem.ReadAllText(InstallPath & "\version")
+                If Not (Convert.ToInt32(currentMP) < Convert.ToInt32(latestMP)) Then
+                    Dim token = "FrangaCraft_Cracked_Launcher"
+                    Dim mcUser As String = TxtBox_CrackedUser.Text()
+                    Dim McWidth As String = "1280"
+                    Dim McHeight As String = "720"
+                    Dim RAM As String = "1G"
+                    Dim run As String = InstallPath & "\1STUFF\direct.bat " & InstallPath & " " & mcUser & " " & token & " " & RAM
+                    'My.Computer.FileSystem.WriteAllText("./run.bat", run, True)
+                    Shell(run, vbNormalFocus)
+                    If closeLauncher Then
+
+                        Me.Close()
+                    End If
+                Else 'If you don't have the latest modpack
+
+                    If MsgBox("You don't have the latest version of the modpack! Want to update?", MsgBoxStyle.YesNo, "Error") = MsgBoxResult.Yes Then
+                        Update()
+                    End If
+                End If
+            Else 'If you don't have the modpack at all
+                If MsgBox("You don't have the modpack! Want to install it?", MsgBoxStyle.YesNo, "Error") = MsgBoxResult.Yes Then
+                    Download()
+                End If
+            End If
+        Else
+            My.Computer.FileSystem.CreateDirectory(InstallPath)
+        End If
+    End Sub
+
+    Private Sub Btn_save_settings_Click(sender As Object, e As EventArgs) Handles Btn_save_settings.Click
+        'GetSettingItem("", "")
+    End Sub
+
+
+
+    'Random buttons
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Btn_Website.Click
+        System.Diagnostics.Process.Start("http://mc.franga2000.com")
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Btn_GitHub.Click
+        System.Diagnostics.Process.Start("http://mc.franga2000.com")
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Btn_MP_info.Click
+        System.Diagnostics.Process.Start("http://mc.franga2000.com")
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Btn_credits.Click
+        System.Diagnostics.Process.Start("http://mc.franga2000.com")
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Btn_about.Click
+        System.Diagnostics.Process.Start("http://mc.franga2000.com")
     End Sub
 End Class
